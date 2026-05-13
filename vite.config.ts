cbbf731cc0b2cff15ca68644990575580f65b250
GEETA-COOL <geeta07.2014@gmail.com>
2026-05-13 17:38:22 +0530

    basic repo

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig , UserConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+     // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+    // hey! 👋 over here
+    globals: true,
+    setupFiles: './tests/setup.ts', // assuming the test folder is in the root of our project
+  }
+} as UserConfig)
