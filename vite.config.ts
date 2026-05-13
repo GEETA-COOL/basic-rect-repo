9635e5efb6c6be6ce0717450182757a15a8c87cc
GEETA-COOL <geeta07.2014@gmail.com>
2026-05-13 18:44:33 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig , UserConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
      // 👋 add the line below to add jsdom to vite
     environment: 'jsdom',
